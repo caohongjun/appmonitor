301e63604af3c4794f59d0881f92a599bf7aaee6
caohongjun <caohongjun@users.noreply.github.com>
2026-02-19 16:46:43 +0800

    删除config.py中的飞书配置并允许上传config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+"""
+配置文件
+"""
+
+# App Store 配置
+APP_STORE_RSS_URL = "https://itunes.apple.com/us/rss/topfreeapplications/limit=100/json"
+
+# Google Play 配置
+GOOGLE_PLAY_COUNTRY = "us"
+GOOGLE_PLAY_CATEGORY = "APPLICATION"
+GOOGLE_PLAY_COLLECTION = "TOP_FREE"  # 可选: TOP_FREE, TOP_PAID, TRENDING
